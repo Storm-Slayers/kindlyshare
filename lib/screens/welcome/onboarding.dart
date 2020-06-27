766b45b660e58ebc5de07ef196f9f58f45058b4b
sughtech <sughtech@aol.com>
2020-06-28 00:55:32 +0100

    created splash, onboarding and welcome files

---
+++ lib/screens/welcome/onboarding.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Onboarding extends StatefulWidget {
+  Onboarding({Key key}) : super(key: key);
+
+  @override
+  _OnboardingState createState() => _OnboardingState();
+}
+
+class _OnboardingState extends State<Onboarding> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+       child: null,
+    );
+  }
+}
